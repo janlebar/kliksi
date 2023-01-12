046f547f8a953ebc5ea0c5f328b6f3bac9bbf012
Jan_Lebar <janstefanlebar@gmail.com>
2023-01-12 15:49:01 +0100

    test

--- kliksi.php
+++ kliksi.php
@@ -6,6 +6,12 @@ foreach ($books as $book) {
   echo $book->getAttribute('id');
 }
 
+$xml = simplexml_load_string($xmlCode, 'SimpleXMLElement', LIBXML_NOCDATA);
+$data = array();
+$data['RIZDDZStevilka1'] = (string) $xml->Izvajalec->RIZDDZStevilka;
+$data['RIZDDZStevilka2'] = (string) $xml->Zdravnik->RIZDDZStevilka;
+$data['Ime1'] = (string) $xml->Zdravnik->ImeZdravnika->Ime1;
+$data['Priimek1'] = (string) $xml->Zdravnik->ImeZdravnika->Priimek1;
 
 
 $xml = simplexml_load_string($xml);
@@ -13,7 +19,8 @@ $arr = array();
 foreach($xml->Podatki->Zdravnik->ImeZdravnika as $value)
 {
     $arr[] = (string)$value->Priimek1;
-    $arr[] = (string)$value->Ime11;
+    $arr[] = (string)$value->Ime1;
+    $arr[] = (string)$value->Ime2;
 }
 print_r($arr);
 ?>
